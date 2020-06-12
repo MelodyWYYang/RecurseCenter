5aab219e445c8befd19e03248725a21ea40c44f5
Louis Kossuth Scheffer V <louis.scheffer@mail.utoronto.ca>
2020-06-12 17:32:33 -0400

    Started itemValidationRequest request with 2 arg and 3 arg constructors. Also added getters for all instance variables and a setter for the item description.

--- phase1/src/itemValidationRequest.java
+++ phase1/src/itemValidationRequest.java
@@ -1,2 +1,31 @@
 public class itemValidationRequest {
+    protected user owner;
+    protected item obj;
+    protected String description;
+
+    public itemValidationRequest(user owner, item obj, String desc){
+        this.description = desc;
+        this.obj = obj;
+        this.owner = owner;
+    }
+    public itemValidationRequest(user owner, item obj){
+        this.obj = obj;
+        this.owner = owner;
+    }
+
+    public item getObj() {
+        return obj;
+    }
+
+    public user getOwner() {
+        return owner;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
