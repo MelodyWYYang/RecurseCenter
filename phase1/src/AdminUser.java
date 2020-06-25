b8250af58fc2fd7130140b9f125bc110924e7825
liangt2001 <64676038+liangt2001@users.noreply.github.com>
2020-06-25 16:48:55 -0400

    Updates on AdminUser.java

--- phase1/src/AdminUser.java
+++ phase1/src/AdminUser.java
@@ -1,3 +1,27 @@
+import javax.lang.model.type.NullType;
+import java.util.ArrayList;
+import java.util.Optional;
+
 public class AdminUser {
     //author: ___________ in group 0110 for CSC207H1 summer 2020 project
+
+    private String username;
+    private String password;
+
+    private ArrayList<ItemValidationRequest> itemValidationQueue;
+    private ArrayList<User> accountsToFreezeQueue;
+
+    protected Optional<Item> pollValidationRequest() {
+        if (itemValidationQueue.size() == 0){
+            return Optional.empty();
+        }
+
+    }
+
+    public void dequeueAndFreeze(ArrayList<User> accountsToFreezeQueue){
+        User user = accountsToFreezeQueue.get(0);
+        user.isFrozen = true;
+        accountsToFreezeQueue.remove(0);
+    }
+
 }
