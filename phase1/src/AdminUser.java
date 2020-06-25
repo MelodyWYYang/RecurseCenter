81f75752f874e1e0d2ba77f6a278f203653364dc
Jinyu Liu <alexjinyu.liu@mail.utoronto.ca>
2020-06-25 16:42:48 -0400

    Implemented basics for UserManager and added itemValidationQueue for AdminUser

--- phase1/src/AdminUser.java
+++ phase1/src/AdminUser.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+
 public class AdminUser {
     //author: ___________ in group 0110 for CSC207H1 summer 2020 project
+    protected static ArrayList<Item> itemValidationQueue;
 }
