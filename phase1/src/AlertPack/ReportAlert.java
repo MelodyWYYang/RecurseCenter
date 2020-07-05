b8bed46a03eb0e7dd16ea5ca7471c945b4daf0e5
razdanri <riya.razdan@mail.utoronto.ca>
2020-07-05 16:46:56 -0400

    included report description to ReportAlert

--- phase1/src/AlertPack/ReportAlert.java
+++ phase1/src/AlertPack/ReportAlert.java
@@ -8,11 +8,14 @@ public class ReportAlert extends AdminAlert implements Serializable {
     protected String senderUserName; // username of the user who is sending the report
     protected String reportedUserName; // username of the user who is being reported
     protected boolean isTradeComplete;
+    protected String reportDescription; // provision to describe why the report is being sent
 
-    public ReportAlert(String senderUserName, String reportedUserName, boolean isTradeComplete) {
+    public ReportAlert(String senderUserName, String reportedUserName, boolean isTradeComplete,
+                       String reportDescription) {
         this.senderUserName = senderUserName;
         this.reportedUserName = reportedUserName;
         this.isTradeComplete = isTradeComplete;
+        this.reportDescription = reportDescription;
     }
 
     /**
@@ -41,11 +44,20 @@ public class ReportAlert extends AdminAlert implements Serializable {
 
     /**
      *
+     * @return the description by the sending user as to why the reported user is being reported.
+     */
+    public String getReportDescription() {
+        return reportDescription;
+    }
+
+    /**
+     *
      * @return the alert text.
      */
     @Override
     public String toString() {
-        return senderUserName + " has reported user " + reportedUserName + " whose trade status is " + isTradeComplete;
+        return senderUserName + " has reported user " + reportedUserName + " whose trade status is " + isTradeComplete
+                + "\n" + "Details: " + reportDescription;
     }
 
 }
